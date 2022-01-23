a8a5f0b5f9ca18e08d487de56dd70ca101c0c3ce
Pierre Lingat <pierre@helloworld.college>
2022-01-23 15:35:11 +1100

    Skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled, { keyframes } from 'styled-components';
+import { tada } from 'react-animations';
+
+const someAnimation = keyframes`${tada}`;
+
+const BouncyDiv = styled.div`
+  animation: 2s ${someAnimation};
+  text-align: center
+
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+ <div>
+  <BouncyDiv>
+       <h1>Portfolio!</h1>
+       <h2> Hello </h2>
+    </BouncyDiv>
     </div>
+      
   );
 }
 
